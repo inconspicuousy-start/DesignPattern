c50970e34f9435e67da6b3773a14c1baf202c888
inconspicuousy <844579582@qq.com>
2020-08-25 19:21:04 +0800

    添加ocp原则部分代码

---
+++ Principle/src/com/inconspicuousy/principle/ocp/error/OcpErrorExample.java
@@ -0,0 +1,22 @@
+package com.inconspicuousy.principle.ocp.error;
+
+/* 基类 */
+class Shape {
+    /** 形状的类型 */
+    int type;
+}
+
+class Circle extends Shape {
+
+}
+
+
+
+
+
+/**
+ * 开放关闭原则错误案例
+ * @author peng.yi
+ */
+public class OcpErrorExample {
+}
